5accf6ad45d2e01f4005fb8629f387c10dd773e9
Pedro Henrique <contatopedrohq@gmail.com>
2023-11-09 20:51:48 -0300

    Lógica para leitura do arquivo.csv completa

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,8 +3,12 @@ package application;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
+
+import entities.Reader;
 
 public class Program {
 
@@ -12,49 +16,45 @@ public class Program {
 		
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner (System.in);
-		
-		System.out.println("Bem-vindo ao FoodFacility! Vamos apresentar os Food Trucks disponíveis com base na sua latitude e longitude.\nPressione enter para continuar...");
-		sc.nextLine();
-		
-		System.out.print("Digite a latitude: ");
-		Double latitude = sc.nextDouble();
-		
-		System.out.print("Digite a longitude: ");
-		Double longitude = sc.nextDouble();
-		
+		List<Reader> list = new ArrayList<Reader>();
 		
 		String path = "C:\\temp\\Mobile_Food_Facility_Permit.csv";
-		FileReader fr = null;
-		BufferedReader br = null;
+		String linha = "";
+		String divisor = ",";
 		
-		try {
-			fr = new FileReader(path);
-			br = new BufferedReader(fr);
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
 
-			String line = br.readLine();
-
-			while (line != null) {
-				System.out.println(line);
-				line = br.readLine();
+			
+			linha = br.readLine();
+			while ((linha = br.readLine()) != null) {
+				
+				String[] vect = linha.split(divisor);
+				String nome = vect[1];
+				String tipo = vect[2];
+				String endereco = vect[5];
+				String tipoCardapio = vect[11];
+				Double latitude = null;
+				Double longitude = null;
+ 
+				Reader reader = new Reader(nome, tipo, endereco, tipoCardapio, latitude, longitude);
+				list.add(reader);
+				
 			}
+			
+			System.out.println("Food Trucks: ");
+			
+			for(Reader reader : list) {
+				
+				System.out.println(reader);
+			}
+			
 		} catch (IOException e) {
 			System.out.println("Error: " + e.getMessage());
-		} finally {
-			try {
-				if (br != null) {
-					br.close();
-				}
-				if (fr != null) {
-					fr.close();
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
 		}
-		
-		
-		
+
 		sc.close();
 	}
 
 }
+
+
--- src/entities/Reader.java
+++ src/entities/Reader.java
@@ -6,59 +6,80 @@ import java.io.IOException;
 
 public class Reader {
 	
+	private String nome;
+	private String tipo;
+	private String endereco;
+	private String tipoCardapio;
 	private Double latitude;
 	private Double longitude;
 	
 	public Reader() {
 	}
 
-	public Reader(Double latitude, Double longitude) {
+	public Reader(String nome, String tipo, String endereco, String tipoCardapio, Double latitude, Double longitude) {
+		this.nome = nome;
+		this.tipo = tipo;
+		this.endereco = endereco;
+		this.tipoCardapio = tipoCardapio;
 		this.latitude = latitude;
 		this.longitude = longitude;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getTipoCardapio() {
+		return tipoCardapio;
+	}
+
+	public void setTipoCardapio(String tipoCardapio) {
+		this.tipoCardapio = tipoCardapio;
 	}
 
 	public Double getLatitude() {
 		return latitude;
 	}
 
+	public void setLatitude(Double latitude) {
+		this.latitude = latitude;
+	}
 
 	public Double getLongitude() {
 		return longitude;
 	}
 
-	public void reader() {
-		
-		String path = "C:\\temp\\Mobile_Food_Facility_Permit.csv";
-		FileReader fr = null;
-		BufferedReader br = null;
-		
-		try {
-			fr = new FileReader(path);
-			br = new BufferedReader(fr);
-			
-			String line = br.readLine();
-			
-			while (line != null  ) {
-				System.out.println(line);
-				line = br.readLine();
-			}
-		}
-		catch (IOException e) {
-			System.out.println("Error: " + e.getMessage());
-		}
-		finally {
-				try {
-				if (br != null) {
-					br.close();
-				}
-				if (fr != null) {
-					fr.close();
-				}
-			} catch(IOException e) {
-				e.printStackTrace();
-			}
- 		}
-		
+	public void setLongitude(Double longitude) {
+		this.longitude = longitude;
+	}
+
+	@Override
+	public String toString() {
+		return "[nome=" + nome + ", tipo=" + tipo + ", endereco=" + endereco + ", tipoCardapio=" + tipoCardapio
+				+ ", latitude=" + String.format("%.2f", latitude) + ", longitude=" + String.format("%.3f", longitude)  + "]";
+	}
+
+	
 	}
 	
-}
+
